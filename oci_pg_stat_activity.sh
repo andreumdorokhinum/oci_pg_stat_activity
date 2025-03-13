23052adc96fd69d1a8aa2bc34176ffed57f1db67
andreumdorokhinum <40900000+andreumdorokhinum@users.noreply.github.com>
2025-03-13 14:28:31 +0100

    Add files via upload

---
+++ oci_pg_stat_activity.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set_notification_threshold_timestamp(){
+    printf "notification_threshold_timestamp=\"${1} ${2}\"\n" >> /etc/environment
+}
+
+get_vars(){
+    source /etc/environment
+}
+
+flu_vars(){
+    tmp=$(sed -e '/notification_threshold_timestamp=.*/d' /etc/environment)
+    echo "${tmp}" > /etc/environment
+    unset tmp notification_threshold_timestamp
+}
+
+start(){
+    get_vars
+
+    if [[ -z $notification_threshold_timestamp ]]; then
+        set_notification_threshold_timestamp $(date '+%Y-%m-%d %H:%M:%S.%6N%:::z')
+        get_vars
+    fi
+
+    notification_condition_and_timestamp=$(PGPASSWORD="${pwd}" psql --host "${host}" --port 5432 --dbname "${db}" --username "${uname}" <<< "\
+            SELECT MAX(query_start)
+              FROM pg_stat_activity
+             WHERE query_start > '$notification_threshold_timestamp'
+               AND query ILIKE 'INSERT INTO%'
+             LIMIT 1;
+             " | grep -E '\b[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}\+[0-9]{2}\b')
+
+    if [[ -n $notification_condition_and_timestamp ]]; then
+        oci ons message publish --title "ALERT!" --body "There is a big query running in your DB!" --topic-id ocid1.onstopic.oc1.aa-bbbbbbbbb-1.cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+        echo "fire in the hall!"
+        flu_vars
+        set_notification_threshold_timestamp $notification_condition_and_timestamp
+        unset notification_condition_and_timestamp
+    fi
+}
+
+stop(){
+    flu_vars
+}
+
+case $1 in
+  start|stop) "$1" ;;
+esac
